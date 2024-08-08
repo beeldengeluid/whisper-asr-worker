5441450bef0913d04f1b37b7ef102e4808dfec95
Sara Veldhoen <s.veldhoen@beeldengeluid.nl>
2024-08-08 09:46:54 +0200

    Renamed app to service

--- server.py
+++ server.py
@@ -1,3 +1,3 @@
 from src.service import create_app
 
-app = create_app()
+service = create_app()
--- src/service/__init__.py
+++ src/service/__init__.py
@@ -1,32 +1,32 @@
 from flask import Flask
 from .rest_api import api
 # from flask_cors import CORS
-from flask import url_for  # , send_from_directory
+from flask import url_for, send_from_directory
 from .health_check import HealthCheckProxy
 
 
 def create_app():
-    app = Flask("search")
-    app.config["DEBUG"] = True
-    # CORS(app)
+    service = Flask("transcribe")
+    service.config["DEBUG"] = True
+    # CORS(service)
 
-    app.logger.info("initializing api")
+    service.logger.info("initializing api")
     api.init_app(
-        app,
-        title="Beeld en Geluid Search API",
-        description="Access the various collections hosted in the B&G laboratory",
+        service,
+        title="Beeld en Geluid Transcription API",
+        description="Transcribe an audio file using Whisper ASR",
     )
 
     @property  # type: ignore
     def specs_url(self):
-        app.logger.debug("Entered function")
+        service.logger.debug("Entered function")
         return url_for(self.endpoint("specs"), _external=True, _scheme="https")
 
-    # @app.route("/robots.txt")
-    # def static_from_root():
-    #     return send_from_directory(app.static_folder, "robots.txt")
+    @service.route("/robots.txt")
+    def static_from_root():
+        return send_from_directory(service.static_folder, "robots.txt")
 
     # wires up the ping and ready check
-    HealthCheckProxy(app)
+    HealthCheckProxy(service)
 
-    return app
+    return service
