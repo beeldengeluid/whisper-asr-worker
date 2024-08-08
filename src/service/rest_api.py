b0819c301eaceb611e45d757e001f654bbb3af0a
Sara Veldhoen <s.veldhoen@beeldengeluid.nl>
2024-08-08 09:48:00 +0200

    Started API definition

--- src/service/rest_api.py
+++ src/service/rest_api.py
@@ -1,5 +1,18 @@
-from flask_restx import Api  # type: ignore
+from flask_restx import Api, Namespace  # type: ignore
 
 apiVersion = "v1.1"
 basePath = "/api/%s" % apiVersion
 api = Api(version=apiVersion)
+
+
+transcribe_api = Namespace("Transcribe", description="Transcribe audio file")
+
+transcribe_response = api.model(
+    "TranscribeResponse",
+    {
+        "task_id": 'this_is_not_a_valid_id'
+    })
+
+
+api.add_namespace(transcribe_api, path="%s/transcribe" % basePath)
+
