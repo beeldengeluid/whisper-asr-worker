7652b01e5736025028166759fafa3f0b0f032180
Dragoș <balandragos5555@gmail.com>
2024-09-10 15:43:46 +0200

    Formatting

--- model_download.py
+++ model_download.py
@@ -15,23 +15,15 @@ def check_model_availability() -> bool:
         logger.info("Model not found locally, attempting to download from S3")
         if not validate_s3_uri(w_model):
             logger.info("No S3 URI detected")
-            logger.info(
-                f"Downloading version {w_model} from Huggingface instead"
-            )
+            logger.info(f"Downloading version {w_model} from Huggingface instead")
             return False
         s3 = S3Store(s3_endpoint_url)
         bucket, object_name = parse_s3_uri(w_model)
-        success = s3.download_file(
-            bucket, object_name, model_base_dir
-        )
+        success = s3.download_file(bucket, object_name, model_base_dir)
         if not success:
-            logger.error(
-                f"Could not download {w_model} into {model_base_dir}"
-            )
+            logger.error(f"Could not download {w_model} into {model_base_dir}")
             return False
-        logger.info(
-            f"Downloaded {w_model} into {model_base_dir}"
-        )
+        logger.info(f"Downloaded {w_model} into {model_base_dir}")
         logger.info("Extracting the model")
         tar_path = model_base_dir + "/" + object_name
         with tarfile.open(tar_path) as tar:
