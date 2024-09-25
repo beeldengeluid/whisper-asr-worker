713dfc5649792e3bfc49b4ad1058ad1f944ebcd8
Jaap Blom <jblom@beeldengeluid.nl>
2024-09-25 10:15:35 +0200

    moved code back in

--- whisper_api.py
+++ whisper_api.py
@@ -1,9 +1,9 @@
 import logging
 from uuid import uuid4
-from fastapi import BackgroundTasks, FastAPI
+from fastapi import BackgroundTasks, FastAPI, HTTPException, status
 from asr import run
 
-# from typing import Optional
+from typing import Optional
 from pydantic import BaseModel
 from base_util import LOG_FORMAT
 
@@ -15,9 +15,37 @@ class Task(BaseModel):
     input_uri: str
     output_uri: str
     id: str | None = None
+    status: str | None = None
 
 
-all_tasks = []
+all_tasks = [
+    {
+        "input_uri": "http://modelhosting.beng.nl/whisper-asr.mp3",
+        "output_uri": "http://modelhosting.beng.nl/assets/whisper-asr",
+        "id": "test1",
+    }
+]
+
+
+def get_task_by_id(task_id: str) -> Optional[dict]:
+    tasks_with_id = list(filter(lambda t: t.get("id", "") == task_id, all_tasks))
+    return tasks_with_id[0] if tasks_with_id else None
+
+
+def get_task_index(task_id: str) -> int:
+    for index, task in enumerate(all_tasks):
+        if task.get("id", "") == task_id:
+            return index
+    return -1
+
+
+# delete after processing is done or if client calls for it
+def delete_task(task_id) -> bool:
+    task_index = get_task_index(task_id)
+    if task_index == -1:
+        return False
+    del all_tasks[task_index]
+    return True
 
 
 def try_whisper(task: Task):
@@ -29,13 +57,45 @@ def try_whisper(task: Task):
     logger.info(f"Successfully ran Whisper for task {task.id}")
 
 
-@api.post("/tasks")
+@api.get("/tasks")
+def get_all_tasks():
+    return {"data": all_tasks}
+
+
+@api.post("/tasks", status_code=status.HTTP_201_CREATED)
 async def create_task(task: Task, background_tasks: BackgroundTasks):
     background_tasks.add_task(try_whisper, task)
     task.id = str(uuid4())
     task_dict = task.dict()
     all_tasks.append(task_dict)
     return {"data": task_dict, "msg": "Successfully added task", "task_id": task.id}
+
+
+@api.get("/tasks/{task_id}")
+async def get_task(task_id: str):
+    task = get_task_by_id(task_id)
+    if not task:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail=f"Task {task_id} not found"
+        )
+    return {"data": task}
+
+
+@api.delete("/tasks/{task_id}")
+async def remove_task(task_id: str):
+    success = delete_task(task_id)
+    if not success:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail=f"Task {task_id} not found"
+        )
+    return {
+        "msg": (
+            f"Successfully deleted task {task_id}"
+            if success
+            else f"Failed to delete task {task_id}"
+        ),
+        "task_id": task_id,
+    }
 
 
 if __name__ == "__main__":
